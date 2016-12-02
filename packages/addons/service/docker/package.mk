3281714e5dd3c27e28628be5859b346cd32502f3
Radostan Riedel <raybuntu@googlemail.com>
2016-12-02 12:44:32 -0800

    docker: add aarch64 support

--- packages/addons/service/docker/package.mk
+++ packages/addons/service/docker/package.mk
@@ -20,7 +20,7 @@ PKG_NAME="docker"
 PKG_VERSION="1.13.0"
 PKG_REV="112"
 PKG_ARCH="any"
-PKG_ADDON_PROJECTS="Generic RPi RPi2"
+PKG_ADDON_PROJECTS="Generic RPi RPi2 imx6 WeTek_Hub WeTek_Play_2"
 PKG_LICENSE="ASL"
 PKG_SITE="http://www.docker.com/"
 PKG_URL="https://github.com/docker/docker/archive/v${PKG_VERSION}.tar.gz"
@@ -58,6 +58,9 @@ configure_target() {
          export GOARM=7
          ;;
       esac
+      ;;
+    aarch64)
+      export GOARCH=arm64
       ;;
   esac
 
